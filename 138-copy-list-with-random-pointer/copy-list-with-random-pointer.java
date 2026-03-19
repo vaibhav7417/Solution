482a6e62783b25a175dfc01547a585aa2d21714d
vaibhav <144949565+vaibhav7417@users.noreply.github.com>
2026-03-19 23:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (9.54%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        
+        Node curr = head;
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        
+        curr = head;
+        Node dummy = new Node(0);
+        Node copyCurr = dummy;
+
+        while (curr != null) {
+            Node copy = curr.next;
+
+            copyCurr.next = copy;
+            copyCurr = copy;
+
+            curr.next = copy.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
